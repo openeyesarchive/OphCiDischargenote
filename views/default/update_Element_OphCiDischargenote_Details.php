5c37e6d219aef4f4fefb4ea2efbd7d13cefd8859
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-07-23 16:43:38 +0100

    fixed dropdown layout issues.

--- views/default/update_Element_OphCiDischargenote_Details.php
+++ views/default/update_Element_OphCiDischargenote_Details.php
@@ -18,19 +18,11 @@
  ?>
 <div class="<?php echo $element->elementType->class_name?>">
 	<h4 class="elementTypeName"><?php  echo $element->elementType->name; ?></h4>
-
-	
-		<?php echo $form->dropDownList($element, 'checklist_completed_id', CHtml::listData(EtOphcidischargenoteDetailsChecklistCompleted::model()->findAll(),'id','name'),array('empty'=>'- Please select -')); ?>
-	
-		<?php echo $form->dropDownList($element, 'ready_to_go_home_id', CHtml::listData(EtOphcidischargenoteDetailsReadyToGoHome::model()->findAll(),'id','name'),array('empty'=>'- Please select -')); ?>
-	
-		<?php echo $form->dropDownList($element, 'district_nurse_contacted_id', CHtml::listData(EtOphcidischargenoteDetailsDistrictNurseContacted::model()->findAll(),'id','name'),array('empty'=>'- Please select -')); ?>
-	
+		<?php echo $form->dropDownList($element, 'checklist_completed_id', CHtml::listData(EtOphcidischargenoteDetailsChecklistCompleted::model()->findAll(),'id','name'),array('empty'=>'- Please select -','layout'=>'vertical')); ?>
+		<?php echo $form->dropDownList($element, 'ready_to_go_home_id', CHtml::listData(EtOphcidischargenoteDetailsReadyToGoHome::model()->findAll(),'id','name'),array('empty'=>'- Please select -','layout'=>'vertical')); ?>
+		<?php echo $form->dropDownList($element, 'district_nurse_contacted_id', CHtml::listData(EtOphcidischargenoteDetailsDistrictNurseContacted::model()->findAll(),'id','name'),array('empty'=>'- Please select -','layout'=>'vertical')); ?>
 		<?php echo $form->textArea($element, 'comments', array('rows' => 6, 'cols' => 80)); ?>
-	
-		<?php echo $form->dropDownList($element, 'following_day_assessment_id', CHtml::listData(EtOphcidischargenoteDetailsFollowingDayAssessment::model()->findAll(),'id','name'),array('empty'=>'- Please select -')); ?>
-	
-		<?php echo $form->dropDownList($element, 'follow_up_id', CHtml::listData(EtOphcidischargenoteDetailsFollowUp::model()->findAll(),'id','name'),array('empty'=>'- Please select -')); ?>
-	
-		<?php echo $form->dropDownList($element, 'information_leaflet_id', CHtml::listData(EtOphcidischargenoteDetailsInformationLeaflet::model()->findAll(),'id','name'),array('empty'=>'- Please select -')); ?>
+		<?php echo $form->dropDownList($element, 'following_day_assessment_id', CHtml::listData(EtOphcidischargenoteDetailsFollowingDayAssessment::model()->findAll(),'id','name'),array('empty'=>'- Please select -','layout'=>'vertical')); ?>
+		<?php echo $form->dropDownList($element, 'follow_up_id', CHtml::listData(EtOphcidischargenoteDetailsFollowUp::model()->findAll(),'id','name'),array('empty'=>'- Please select -','layout'=>'vertical'))?>
+		<?php echo $form->dropDownList($element, 'information_leaflet_id', CHtml::listData(EtOphcidischargenoteDetailsInformationLeaflet::model()->findAll(),'id','name'),array('empty'=>'- Please select -','layout'=>'vertical')); ?>
 	</div>
