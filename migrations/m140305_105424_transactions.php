6ee915ee90abcba0f62c7c96f13b2e04a2d30b3e
Mark <m@rkw.me>
2014-03-05 10:55:05 +0000

    [OE-3987] transactions

---
+++ migrations/m140305_105424_transactions.php
@@ -0,0 +1,42 @@
+<?php
+
+class m140305_105424_transactions extends CDbMigration
+{
+	public $tables = array('et_ophcidischargenote_details_checklist_completed','et_ophcidischargenote_details_district_nurse_contacted','et_ophcidischargenote_details_follow_up','et_ophcidischargenote_details_following_day_assessment','et_ophcidischargenote_details_information_leaflet','et_ophcidischargenote_details_ready_to_go_home','et_ophcidischargenote_details');
+
+	public function up()
+	{
+		foreach ($this->tables as $table) {
+			$this->addColumn($table,'hash','varchar(40) not null');
+			$this->addColumn($table,'transaction_id','int(10) unsigned null');
+			$this->createIndex($table.'_tid',$table,'transaction_id');
+			$this->addForeignKey($table.'_tid',$table,'transaction_id','transaction','id');
+
+			$this->addColumn($table.'_version','hash','varchar(40) not null');
+			$this->addColumn($table.'_version','transaction_id','int(10) unsigned null');
+			$this->addColumn($table.'_version','deleted_transaction_id','int(10) unsigned null');
+			$this->createIndex($table.'_vtid',$table.'_version','transaction_id');
+			$this->addForeignKey($table.'_vtid',$table.'_version','transaction_id','transaction','id');
+			$this->createIndex($table.'_dtid',$table.'_version','deleted_transaction_id');
+			$this->addForeignKey($table.'_dtid',$table.'_version','deleted_transaction_id','transaction','id');
+		}
+	}
+
+	public function down()
+	{
+		foreach ($this->tables as $table) {
+			$this->dropColumn($table,'hash');
+			$this->dropForeignKey($table.'_tid',$table);
+			$this->dropIndex($table.'_tid',$table);
+			$this->dropColumn($table,'transaction_id');
+
+			$this->dropColumn($table.'_version','hash');
+			$this->dropForeignKey($table.'_vtid',$table.'_version');
+			$this->dropIndex($table.'_vtid',$table.'_version');
+			$this->dropColumn($table.'_version','transaction_id');
+			$this->dropForeignKey($table.'_dtid',$table.'_version');
+			$this->dropIndex($table.'_dtid',$table.'_version');
+			$this->dropColumn($table.'_version','deleted_transaction_id');
+		}
+	}
+}
